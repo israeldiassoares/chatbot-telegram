8518041da334c08e2c8b52288144f180e2e36875
Israel Soares <israeldiassoares@gmail.com>
2019-03-18 16:44:29 -0300

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const TelegramBot = require('node-telegram-bot-api');
-
-const TOKEN = 'TOKEN';
-
-const bot = new TelegramBot(TOKEN, { polling: true });
-
-bot.on('new_chat_members', (msg) => {
-  bot.sendMessage(msg.chat.id, `Hey ! ${msg.from.first_name}, bem vindo ! `)
-})
